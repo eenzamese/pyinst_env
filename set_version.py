a197fe8be105202389ec3f44e9ecfc65b87febdc
eenzamese <eenzamese@gmail.com>
2024-12-11 10:59:32 -0800

    args

--- set_version.py
+++ set_version.py
@@ -1,12 +1,17 @@
 import time
 import random
-print(time.ctime())
+import argparse 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--version')
+args = parser.parse_args()
+
 with open('pyinst_env\\ms_version.txt', 'r') as version_file:
   version_content = version_file.read()
 print('######### CURRENT VERSION #########')
 print(version_content)
 version_value = str(random.randint(10000, 99999)) 
-new_version = version_content.replace('__VERSION__', version_value)
+new_version = version_content.replace('__VERSION__', args.version)
 print('######### NEW VERSION #########')
 with open('..\\ms_version_1.txt', 'w') as version_file:
   version_file.write(new_version)
