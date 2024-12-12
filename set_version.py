1a3fe54f0c17016ed62249f3dffd9a473cfd381a
Eenzamese <eenzamese@gmail.com>
2024-12-11 22:31:39 -0800

    Update set_version.py

--- set_version.py
+++ set_version.py
@@ -10,8 +10,8 @@ with open('pyinst_env\\ms_version.txt', 'r') as version_file:
   version_content = version_file.read()
 print('######### CURRENT VERSION #########')
 print(version_content)
-version_value = str(random.randint(10000, 99999)) 
-new_version = version_content.replace('__VERSION__', args.version[-3:])
+version_value = f'11{args.version[-3:]}'
+new_version = version_content.replace('__VERSION__', version_value)
 print('######### NEW VERSION #########')
 with open('..\\ms_version_1.txt', 'w') as version_file:
   version_file.write(new_version)
